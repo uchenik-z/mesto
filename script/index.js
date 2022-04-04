260f9d7fef8bb7e4c6f152a8623f0f700340d765
Nikolay Murzin <zemmlyanin@mail.ru>
2022-04-04 18:20:47 +0500

    Сделать работу над ошибками

--- script/index.js
+++ script/index.js
@@ -2,33 +2,30 @@ const editButton = document.querySelector('.profile__edit-button');
 const modalWindow = document.querySelector('.popup');
 const closeButton = modalWindow.querySelector('.popup__close');
 const popupForm = modalWindow.querySelector('.popup__form');
-const nameInput = popupForm.querySelector('.popup__text_name');
-const jobInput = popupForm.querySelector('.popup__text_job');
+const nameInput = popupForm.querySelector('.popup__text_input_name');
+const jobInput = popupForm.querySelector('.popup__text_input_job');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const saveButton = modalWindow.querySelector('.popup__save');
 
-function togglePopup() {
-  modalWindow.classList.toggle('popup_opened');
+function onPopup() {
+  modalWindow.classList.add('popup_opened');
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
+editButton.addEventListener('click', onPopup);
 
-function overlayClick(event) {
-  if (event.target === event.currentTarget) {
-    togglePopup()
-  }
+function closePopup() {
+  modalWindow.classList.remove('popup_opened');
 }
 
-modalWindow.addEventListener('click', overlayClick);
+closeButton.addEventListener('click', closePopup);
 
 function onSubmit(event) {
   event.preventDefault();
    profileTitle.textContent = nameInput.value;
    profileSubtitle.textContent = jobInput.value;
 
-   togglePopup()
+   closePopup()
 }
 
 popupForm.addEventListener('submit', onSubmit);
