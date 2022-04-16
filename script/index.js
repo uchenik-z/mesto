9b6208bb5447536f129dc1c2199b31736118fc6d
Nicolay Murzin <zemmlyanin@mail.ru>
2022-04-16 18:48:38 +0500

    Добавить карточки с помощью js

--- script/index.js
+++ script/index.js
@@ -1,33 +1,126 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const editButton = document.querySelector('.profile__edit-button');
-const modalWindow = document.querySelector('.popup');
-const closeButton = modalWindow.querySelector('.popup__close');
-const popupForm = modalWindow.querySelector('.popup__form');
-const nameInput = popupForm.querySelector('.popup__text_input_name');
-const jobInput = popupForm.querySelector('.popup__text_input_job');
+const modalWindowProfile = document.querySelector('.popup_place_profile');
+const closeButtonProfile = modalWindowProfile.querySelector('.popup__close');
+const popupFormProfile = modalWindowProfile.querySelector('.popup__form');
+const nameInput = popupFormProfile.querySelector('.popup__text_input_name');
+const jobInput = popupFormProfile.querySelector('.popup__text_input_job');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const saveButton = modalWindow.querySelector('.popup__save');
+const saveButton = modalWindowProfile.querySelector('.popup__save');
 
-function onPopup() {
+const addButton = document.querySelector('.profile__add-button');
+const modalWindowCard = document.querySelector('.popup_place_card');
+const closeButtonCard = modalWindowCard.querySelector('.popup__close');
+const popupFormCard = modalWindowCard.querySelector('.popup__form');
+const titleInput = popupFormCard.querySelector('.popup__text_input_title');
+const linkInput = popupFormCard.querySelector('.popup__text_input_link');
+
+const elementList = document.querySelector('.element');
+const templateElement = document.querySelector('.template-element');
+
+function onPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function profileInput() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-  modalWindow.classList.add('popup_opened');
 }
 
-editButton.addEventListener('click', onPopup);
+editButton.addEventListener('click', function() {
+  onPopup(modalWindowProfile);
+  profileInput();
+});
 
-function closePopup() {
-  modalWindow.classList.remove('popup_opened');
+addButton.addEventListener('click', function() {
+  onPopup(modalWindowCard);
+});
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener('click', closePopup);
+closeButtonProfile.addEventListener('click', function() {
+  closePopup(modalWindowProfile);
+});
 
-function onSubmit(event) {
+closeButtonCard.addEventListener('click', function() {
+  closePopup(modalWindowCard);
+});
+
+function onSubmitProfile(event) {
   event.preventDefault();
    profileTitle.textContent = nameInput.value;
    profileSubtitle.textContent = jobInput.value;
-
-   closePopup()
+   closePopup(modalWindowProfile);
 }
 
-popupForm.addEventListener('submit', onSubmit);
+popupFormProfile.addEventListener('submit', onSubmitProfile);
+
+function onSubmitCard(event) {
+  event.preventDefault();
+  const titleInput = popupFormCard.querySelector('.popup__text_input_title').value;
+  const linkInput = popupFormCard.querySelector('.popup__text_input_link').value;
+  const element = getElementHTML({name: titleInput, link: linkInput});
+  elementList.prepend(element);
+  closePopup(modalWindowCard);
+}
+
+popupFormCard.addEventListener('submit', onSubmitCard);
+
+
+
+
+
+function render() {
+  const elementHTML = initialCards.map(getElementHTML);
+  elementList.append(...elementHTML);
+}
+
+function getElementHTML (item) {
+  const getElementTemplate = templateElement.content.cloneNode(true);
+  const name = getElementTemplate.querySelector('.element__title');
+  name.textContent = item.name;
+  const link = getElementTemplate.querySelector('.element__image');
+  link.src = item.link;
+
+  return getElementTemplate;
+}
+
+render();
+
+
+
+
+
+
+
+
+
