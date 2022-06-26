1146924918d76c9febe3de4396742712f2c7008a
Nicolay Murzin <zemmlyanin@mail.ru>
2022-06-26 18:34:09 +0500

    Рассортировать по папкам

---
+++ src/pages/index.js
@@ -0,0 +1,73 @@
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import UserInfo from '../components/UserInfo.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import { initialCards, editButton, modalWindowProfile, popupFormProfile, nameInput, jobInput, addButton, modalWindowCard, popupFormCard } from '../utils/constants.js';
+import { config } from '../utils/utils.js';
+
+
+
+const cardFormValidator = new FormValidator(config, popupFormCard);
+const profileFormValidator = new FormValidator(config, popupFormProfile);
+cardFormValidator.enableValidation();
+profileFormValidator.enableValidation();
+
+
+function addCard(data) {
+  const card = new Card (data, '.template-element', handlePhotoClick);
+  const cardElement = card.generateCard();
+  return cardElement;
+}
+
+
+const section = new Section({ items: initialCards,
+  renderer: (item) => {
+    const card = addCard(item);
+    section.addItem(card);
+  }
+}, '.element');
+
+section.renderItems();
+
+
+const popupWithImage = new PopupWithImage('.popup_card_image');
+popupWithImage.setEventListeners();
+function handlePhotoClick({name, link}) {
+  popupWithImage.open({name, link});
+};
+
+
+const cardForm = new PopupWithForm('.popup_place_card', {submitHandler: (data) => {
+  const input = {name: data.title, link: data.link};
+  const elementCard = addCard(input);
+  section.addItem(elementCard);
+  cardForm.close();
+}});
+
+cardForm.setEventListeners();
+
+addButton.addEventListener('click', function() {
+  cardForm.open();
+  cardFormValidator.validationReset();
+});
+
+
+const userInfo = new UserInfo({name: '.profile__title', job: '.profile__subtitle'});
+
+
+const profileForm = new PopupWithForm('.popup_place_profile', {submitHandler: (data) => {
+  userInfo.setUserInfo({name: data.name, job: data.job});
+  profileForm.close();
+}});
+
+profileForm.setEventListeners();
+
+editButton.addEventListener('click', function() {
+  profileForm.open();
+  const userData = userInfo.getUserInfo();
+  nameInput.value = userData.name;
+  jobInput.value = userData.job;
+  profileFormValidator.validationReset();
+});
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -24,3 +24,13 @@ export const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
+export const editButton = document.querySelector('.profile__edit-button');
+export const modalWindowProfile = document.querySelector('.popup_place_profile');
+export const popupFormProfile = modalWindowProfile.querySelector('.popup__form');
+export const nameInput = popupFormProfile.querySelector('.popup__input_type_name');
+export const jobInput = popupFormProfile.querySelector('.popup__input_type_job');
+
+export const addButton = document.querySelector('.profile__add-button');
+export const modalWindowCard = document.querySelector('.popup_place_card');
+export const popupFormCard = modalWindowCard.querySelector('.popup__form');
